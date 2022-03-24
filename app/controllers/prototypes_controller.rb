bd603327e7606789c5709797a7c7294f82abdae9
mi-ma-ri <michihiros93@gmail.com>
2022-03-24 18:18:30 +0900

    protospace修正 #2

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -22,9 +22,8 @@ class PrototypesController < ApplicationController
 
   def update
     @prototype = Prototype.find(params[:id])
-    @prototype.update(prototype_params)
-    if 
-      redirect_to root_path
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
     else
      render :new
     end
